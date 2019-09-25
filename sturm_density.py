8a1ea57315cbcaff59bb862beca77cb5796b11fe
jmichellehu <jmhu@uw.edu>
2019-09-25 09:58:01 -0700

    Adding sturm density code and notebook

--- sturm_density.py
+++ sturm_density.py
@@ -1,67 +1,42 @@
 import numpy as np
 
 # Bulk density model based on climate classes of seasonal snow
-DOY_i = -92          # specify the day of year of interest (-92 = Oct 1; +181 = June 30)
-h_i = 1.4           # ith observation of snow_depth in meters
-
-# Determine model parameters based on climate snow class
+# Sturm's model parameters by snow class - Table 4 in Sturm et al., 2010
 # Ephemeral snow lacked sufficient systematic measurements, was excluded
 # rho_max is the maximum bulk density of the season
 # rho_init is the initial seasonal bulk density
 # k1 is the densification parameter for snow_depth
 # k2 is the densification parameter for DOY
-climate_class = 3   # values range 1-5 based on snow class
+def get_sturm_density():
+    snow_classes = ["Alpine", "Maritime", "Prairie", "Tundra", "Taiga"]
+    rho_maxes = [0.5975, 0.5979, 0.5940, 0.3630, 0.2170]
+    rho_inits = [0.2237, 0.2578, 0.2332, 0.2425, 0.2170]
+    k1s = [0.0012, 0.0010, 0.0016, 0.0029, 0.0000]
+    k2s = [0.0038, 0.0038, 0.0031, 0.0049, 0.0000]
 
-if climate_class == 1:
-    # Alpine (1)
-    snow_class = "Alpine"
-    rho_max = 0.5975
-    rho_init = 0.2237
-    k1 = 0.0012
-    k2 = 0.0038
-elif climate_class == 2:
-    # Maritime (2)
-    snow_class = "Maritime"
-    rho_max = 0.5979
-    rho_init = 0.2578
-    k1 = 0.0010
-    k2 = 0.0038
-elif climate_class == 3:
-    # Prairie (3)
-    snow_class = "Prairie"
-    rho_max = 0.5940
-    rho_init = 0.2332
-    k1 = 0.0016
-    k2 = 0.0031
-elif climate_class == 4:
-    # Tundra (4)
-    snow_class = "Tundra"
-    rho_max = 0.3630
-    rho_init = 0.2425
-    k1 = 0.0029
-    k2 = 0.0049
-elif climate_class == 5:
-    # Taiga (5)
-    snow_class = "Taiga"
-    rho_max = 0.2170
-    rho_init = 0.2170
-    k1 = 0.0000
-    k2 = 0.0000
-else:
-    # display error
-    print("Incorrect climate class value.")
+    # Will need to extract day of year, pre-existing snow depth, and climate class based on image metadata (location, date of collection, etc.)
+    DOY_i = -92          # specify the day of year of interest (-92 = Oct 1; +181 = June 30)
+    h_i = 1.4           # ith observation of snow_depth in meters
 
-# t = snow deposition history
-# rho_b = function of h_s, t, and initial snow layer density
-rho_b_model = (rho_max - rho_init) * (1-np.exp(-k1 * h_i - k2 * DOY_i)) + rho_init
+    climate_class = 0   # values range 0-4 based on snow class
 
-print(rho_b_model)
+    snow_class = snow_classes[climate_class]
+    rho_max = rho_maxes[climate_class]
+    rho_init = rho_inits[climate_class]
+    k1 = k1s[climate_class]
+    k2 = k2s[climate_class]
 
-# Basic SWE calculation using snow_depth and bulk_density
-h_s = 1             # snow_depth in meters
-rho_w = 1           # density_of_water in grams/cubic cm
-# rho_b = 0.3         # bulk_density in grams/cubic cm
+    # t = snow deposition history
+    # rho_b = function of h_s, t, and initial snow layer density
+    rho_b_model = (rho_max - rho_init) * (1-np.exp(-k1 * h_i - k2 * DOY_i)) + rho_init
+    # print(rho_b_model)
+    return rho_b_model
 
-swe = h_s * rho_b_model / rho_w
-print("SWE from basic calculations using bulk density is "
-+ str(format(swe, '.2f')) + " meters in the " + snow_class + " snow class.")
+    # Basic SWE calculation using snow_depth and bulk_density
+    # h_s = 1             # snow_depth in meters
+    # rho_w = 1           # density_of_water in grams/cubic cm
+    # rho_b = 0.3         # bulk_density in grams/cubic cm
+
+    # swe = h_s * rho_b_model / rho_w
+    # print("SWE from basic calculations using bulk density based on snow class is "
+    # + str(format(swe, '.2f')) + " meters in the " + snow_class + " snow class.")
